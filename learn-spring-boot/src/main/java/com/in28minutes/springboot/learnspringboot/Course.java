6c4192a518bd247e8c0f47f653b0190ce25d01c3
harshit <harshitkumar9548@gmail.com>
2024-01-24 16:45:38 +0530

    Springboot starter project

---
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/Course.java
@@ -0,0 +1,41 @@
+package com.in28minutes.springboot.learnspringboot;
+
+public class Course {
+	private long id;
+	private String name;
+	private String author;
+	
+	//constructor
+	
+	Course(long id,String name,String author){
+		super();
+		this.id = id;
+		this.name=name;
+		this.author = author;
+	}
+	
+	//getters
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	//toString methods
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + ", author=" + author + "]";
+	}
+
+
+	
+	
+	
+}
